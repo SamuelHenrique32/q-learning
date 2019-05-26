0fe7c30fa0862ed443139f767db8813246636051
SamuelHenrique32 <shdalmas@ucs.br>
2019-05-25 21:24:22 -0300

    Tabuleiro OK

---
+++ src/tabuleiro.js
@@ -0,0 +1,49 @@
+//tabuleiro recebe matriz com 0's e 1's e renderiza toda vez que recebe
+//nova matriz
+
+import React, { Component } from "react";
+import { withStyles } from '@material-ui/core/styles';
+
+//Material UI
+import Grid from "@material-ui/core/Grid";
+
+const styles = {
+    grid:{
+        width:"100%",
+        height:"100%"
+    },
+    Colorido:{
+        minHeight:"10vh",
+        backgroundColor:"red"
+    }
+}
+
+class Tabuleiro extends Component {   //componente do React
+    constructor(props){
+        super(props)
+
+        this.state = {
+            tabuleiro:[[1,0,1],[0,1,0]]
+        }
+        this.props = props;
+    }
+    render(){
+      const {classes} = this.props
+      return(
+        <Grid container className={classes.grid}>
+        <Grid item xs={12}>  
+        {this.state.tabuleiro.map((v)=>{
+            let val = v;
+            return (<Grid container>
+                {val.map((v2)=>{
+                    let value = v2;
+                    console.log(value);
+                    return(<Grid item xs={1} className={ value === 1 ? classes.Colorido : ""}></Grid>)
+                })}
+            </Grid>);
+        })}
+        </Grid>
+    </Grid>
+    )}  
+}
+export default withStyles(styles)(Tabuleiro);
