4243a44a9186976c7dd64717bd489897952479c3
SamuelHenrique32 <shdalmas@ucs.br>
2019-06-01 23:15:36 -0300

    matrizq OK

--- src/QLearning.js
+++ src/QLearning.js
@@ -30,11 +30,59 @@ class QLearning extends EventEmitter {
         //Cada coluna corresponde a uma ação
         //A posição linha x coluna representa estado/ação
         // Cima[0] Baixo[1] Direita[2] Esquerda[3]
-        this.matrizq = [[-1,undefined,-100,undefined],
-                        [-1,undefined,-1,undefined]]
+        this.matrizq = [[-1,undefined,-100,undefined],      //1
+                        [-1,undefined,-1,undefined],        //2
+                        [-1,-1,-1,undefined],               //3
+                        [-1,-1,-100,undefined],             //4
+                        [undefined,-1,-1,undefined],        //5
+                        [undefined,-100,-1,-1],             //6
+                        [-1,-1,-100,-1],                    //7
+                        [-100,-1,-1,-1],                    //8
+                        [-1,-100,-1,-1],                    //9
+                        [-1,undefined,-100,undefined],      //10
+                        [-1,undefined,-100,-100],           //11
+                        [-1,-100,-100,-1],                  //12
+                        [-100,-1,-1,-1],                    //13
+                        [-1,-1,-1,-100],                    //14
+                        [undefined,-100,-1,-1],             //15
+                        [undefined,-1,-1,-1],               //16
+                        [-1,-1,-100,-100],                  //17
+                        [-1,-100,-1,-1],                    //18
+                        [-1,-100,-1,-1],                    //19
+                        [-100,undefined,-100,-100],         //20
+                        [-1,undefined,-100,-100],           //21
+                        [-1,-100,-1,-100],                  //22
+                        [-100,-1,-1,-1],                    //23
+                        [-1,-1,-100,-1],                    //24
+                        [undefined,-100,-1,-1],             //25
+                        [undefined,-100,-1,-1],             //26
+                        [-1,-1,-1,-100],                    //27
+                        [-100,-1,-1,-1],                    //28
+                        [-1,-100,-1,-1],                    //29
+                        [-1,undefined,-100,-100],           //30
+                        [-1,undefined,-100,-100],           //31
+                        [-1,-100,-100,-1],                  //32
+                        [-1,-1,-1,-1],                      //33
+                        [-1,-1,-100,-100],                  //34
+                        [undefined,-1,-1,-1],               //35
+                        [undefined,-100,-1,-1],             //36
+                        [-1,-1,-1,-1],                      //37
+                        [-100,-100,-1,-1],                  //38
+                        [-1,-100,-1,-1],                    //39
+                        [-100,undefined,-100,-100],         //40
+                        [-1,undefined,100,-100],            //41
+                        [-1,-100,-1,-100],                  //42
+                        [-1,-1,-1,-1],                      //43
+                        [-1,-1,-1,-100],                    //44
+                        [undefined,-1,-1,-1],               //45
+                        [undefined,-1,undefined,-1],        //46
+                        [-1,-1,undefined,-1],               //47
+                        [-1,-1,undefined,-1],               //48
+                        [-1,100,undefined,-1],              //49
+                        [-1,undefined,undefined,-100]]      //50
     };
 
-    //Começa a calcular os episódeos
+    //Começa a calcular os episódios
     start(){
         this.isExecuting = true;
     };
